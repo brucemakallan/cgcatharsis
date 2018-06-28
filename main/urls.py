0aef96c6fa157155b84f1020459f4856068cb0a5
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-28 23:26:23 +0300

    Clean up template, Finish Homepage

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,6 @@ from main import views
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'),
     path('dance', views.dance, name='dance'),
     path('draw', views.draw, name='draw'),
-    path('build', views.build, name='build'),
-    path('contact', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,21 +5,9 @@ def index(request):
     return render(request, 'main/index.html')
 
 
-def about(request):
-    return render(request, 'main/about.html')
-
-
 def dance(request):
     return render(request, 'main/dance.html')
 
 
 def draw(request):
     return render(request, 'main/draw.html')
-
-
-def build(request):
-    return render(request, 'main/build.html')
-
-
-def contact(request):
-    return render(request, 'main/contact.html')
