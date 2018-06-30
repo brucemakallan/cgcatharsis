bd67fb2c3eeca9276f23efb2f9581aea59db60a4
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-30 22:53:19 +0300

    Finish Heroku setup

--- cgcatharsis/settings.py
+++ cgcatharsis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5sv7r4t1cjboz7$3qv4&)sdqy#3rzmv%ukzs-d!-#^b(s0x84l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'cgcatharsis.herokuapp.com', 'cgcatharsis.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'cgcatharsis.herokuapp.com', '.cgcatharsis.com']
 
 
 # Application definition
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,11 +121,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# for /static/root/favicon.ico
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root')
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # setup database for Heroku
 db_from_env = dj_database_url.config()
