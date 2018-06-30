f27446c2d84f5500a27111cb7d993a39c666c739
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-30 17:04:23 +0300

    Heroku Setup 3

--- cgcatharsis/settings.py
+++ cgcatharsis/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import dj-database-url
-
 import os
+import dj_database_url  # For Heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
